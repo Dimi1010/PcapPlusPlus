dc4af3979840cb121005754da0192af37f20ce88
Dimitar Krastev <dimi1010100@gmail.com>
2025-08-02 22:25:36 +0300

    Added packet factory for quick packet creation.

---
+++ Tests/Packet++Test-google/Utils/PacketFactory.cpp
@@ -0,0 +1,62 @@
+#include "PacketFactory.hpp"
+
+#include "SystemUtils.h"
+#include "TimespecTimeval.h"
+
+namespace pcpp
+{
+	namespace test
+	{
+		PacketFactory::PacketFactory()
+		{
+			timeval factoryTimeTV;
+			// Initialize factoryTime to the current time
+			gettimeofday(&factoryTimeTV, nullptr);
+			factoryTime = internal::toTimespec(factoryTimeTV);
+		}
+
+		std::unique_ptr<RawPacket> PacketFactory::createFromBuffer(std::unique_ptr<uint8_t[]> buffer,
+		                                                           size_t bufferLen) const
+		{
+			if (buffer == nullptr || bufferLen == 0)
+			{
+				throw std::invalid_argument("Buffer cannot be null and length must be greater than zero");
+			}
+
+			return std::make_unique<RawPacket>(buffer.release(), static_cast<int>(bufferLen), factoryTime, true,
+			                                   defaultLinkType);
+		}
+
+		std::unique_ptr<RawPacket> PacketFactory::createFromBufferNonOwning(std::vector<uint8_t> const& buffer) const
+		{
+			return createFromBufferNonOwning(buffer.data(), buffer.size());
+		}
+
+		std::unique_ptr<RawPacket> PacketFactory::createFromBufferNonOwning(const uint8_t* buffer,
+		                                                                    size_t bufferLen) const
+		{
+			if (buffer == nullptr || bufferLen == 0)
+			{
+				throw std::invalid_argument("Buffer cannot be null and length must be greater than zero");
+			}
+
+			return std::make_unique<RawPacket>(buffer, static_cast<int>(bufferLen), factoryTime, false,
+			                                   defaultLinkType);
+		}
+
+		std::unique_ptr<RawPacket> createPacketFromHexResource(const std::string& resourceName,
+		                                                       const PacketFactory& factory,
+		                                                       TestDataLoader const* dataLoader)
+		{
+			if (dataLoader == nullptr)
+			{
+				// If no data loader is provided, use the current test environment's data loader
+				dataLoader = &PacketTestEnvironment::getCurrent().getDataLoader();
+			}
+
+			size_t bufferLen;
+			auto buffer = dataLoader->loadResourceToNewBuffer(resourceName, bufferLen, ResourceType::HexData);
+			return factory.createFromBuffer(std::move(buffer), bufferLen);
+		}
+	}  // namespace test
+}  // namespace pcpp
---
+++ Tests/Packet++Test-google/Utils/PacketFactory.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "pch.h"
+
+#include "RawPacket.h"
+
+namespace pcpp
+{
+	namespace test
+	{
+		class PacketFactory
+		{
+		public:
+			/// @brief The time used for creating packets in the factory.
+			timespec factoryTime;
+			/// @brief The default link layer type for packets created by this factory.
+			LinkLayerType defaultLinkType = LinkLayerType::LINKTYPE_ETHERNET;
+
+			/// @brief Creates a new PacketFactory instance with the current time as factoryTime.
+			PacketFactory();
+
+			// TODO: RawPacket requires a move constructor to return by value efficiently.
+			/// @brief Creates a RawPacket from a vector of bytes.
+			/// @param buffer A unique pointer to a buffer containing the raw packet data.
+			/// @param bufferLen The length of the buffer in bytes.
+			/// @return A RawPacket object created from the buffer.
+			std::unique_ptr<RawPacket> createFromBuffer(std::unique_ptr<uint8_t[]> buffer, size_t bufferLen) const;
+
+			/// @brief Creates a RawPacket from a vector of bytes without taking ownership of the data.
+			/// @param buffer A vector containing the raw packet data.
+			/// @return A RawPacket object created from the buffer.
+			std::unique_ptr<RawPacket> createFromBufferNonOwning(std::vector<uint8_t> const& buffer) const;
+
+			/// @brief Creates a RawPacket from a buffer without taking ownership of the data.
+			/// @param buffer A pointer to the raw packet data.
+			/// @param bufferLen The length of the buffer in bytes.
+			/// @return A RawPacket object created from the buffer.
+			std::unique_ptr<RawPacket> createFromBufferNonOwning(const uint8_t* buffer, size_t bufferLen) const;
+		};
+
+		/// @brief Creates a RawPacket from a resource file.
+		/// @param resourceName The name of the resource file to read the packet data from.
+		/// @param factory The PacketFactory to use for creating the RawPacket.
+		/// @param dataLoader An optional TestDataLoader to use for loading the resource file.
+		///   Uses the test environment loader if not provided.
+		/// @return A RawPacket object created from the resource file.
+		std::unique_ptr<RawPacket> createPacketFromHexResource(const std::string& resourceName,
+		                                                       const PacketFactory& factory = PacketFactory(),
+		                                                       TestDataLoader const* dataLoader = nullptr);
+
+	}  // namespace test
+}  // namespace pcpp
