2bc3916c76e237992d99b6c8085e093000455572
Dimitar Krastev <dimi1010100@gmail.com>
2026-04-13 11:11:15 +0300

    Prototyping

--- Packet++/header/QuicLayer.h
+++ Packet++/header/QuicLayer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Layer.h"
+#include "EndianPortable.h"
 
 // https://datatracker.ietf.org/doc/rfc9000/
 
@@ -9,30 +10,60 @@ namespace pcpp
 	namespace internal
 	{
 #pragma pack(push, 1)
+		struct quic_common_first_byte
+		{
+#if (BYTE_ORDER == LITTLE_ENDIAN)
+			uint8_t formSpecificBits : 6;
+			uint8_t fixedBit : 1;
+			uint8_t headerForm : 1;
+#else
+			uint8_t headerForm : 1;
+			uint8_t fixedBit : 1;
+			uint8_t formSpecificBits : 6;
+#endif
+		};
+		static_assert(sizeof(quic_common_first_byte) == 1, "Size of quic_common_first_byte must be 1 byte");
+
 		/// @brief A structure that represents the fixed part of a long header QUIC packet.
 		///
 		/// This structure excludes the variable-length fields that may follow the fixed header, such as the Destination
 		/// Connection ID, Source Connection ID, and any additional fields specific to the packet type.
 		struct quic_fixed_header_long
 		{
-			/// @brief Should always be 1 for long headers
-			uint8_t headerForm : 1;
-			/// @brief Should always be 1 for valid packets.
-			/// Only version negotiation packets can have this bit set to 0.
-			uint8_t fixedBit : 1;
-			/// @brief The QUIC packet type
-			uint8_t longPacketType : 2;
-			/// @brief Reserved for use by the specific packet type
-			/// @remarks These bits are protected using header protection [QUIC-TLS Section 5.4].
-			uint8_t typeSpecificBits : 4;
+			struct
+			{
+#if (BYTE_ORDER == LITTLE_ENDIAN)
+				/// @brief Reserved for use by the specific packet type
+				/// @remarks These bits are protected using header protection [QUIC-TLS Section 5.4].
+				uint8_t typeSpecificBits : 4;
+				/// @brief The QUIC packet type
+				uint8_t longPacketType : 2;
+				/// @brief Should always be 1 for valid packets.
+				/// Only version negotiation packets can have this bit set to 0.
+				uint8_t fixedBit : 1;
+				/// @brief Should always be 1 for long headers
+				uint8_t headerForm : 1;
+#else
+				/// @brief Should always be 1 for long headers
+				uint8_t headerForm : 1;
+				/// @brief Should always be 1 for valid packets, except
+				/// version negotiation packets can have this bit set to 0.
+				uint8_t fixedBit : 1;
+				/// @brief The QUIC packet type
+				uint8_t longPacketType : 2;
+				/// @brief Reserved for use by the specific packet type
+				/// @remarks These bits are protected using header protection [QUIC-TLS Section 5.4].
+				uint8_t typeSpecificBits : 4;
+#endif
+			} firstByte;
 
 			uint32_t version;
 
 			// Followed by variable-length fields:
-			// - Destination Connection ID Length (8 bits)
-			// - Destination Connection ID (variable length, max 20)
-			// - Source Connection ID Length (8 bits)
-			// - Source Connection ID (variable length, max 20)
+			// - Destination Connection ID Length (1 byte)
+			// - Destination Connection ID (variable length, max 20 bytes)
+			// - Source Connection ID Length (1 byte)
+			// - Source Connection ID (variable length, max 20 bytes)
 			// - Type specific payload (variable length)
 		};
 
@@ -42,8 +73,10 @@ namespace pcpp
 
 			uint8_t destConnLen;
 			uint8_t const* destId;
+
 			uint8_t sourceConnLen;
 			uint8_t const* sourceId;
+
 			uint8_t const* typePayload;
 		};
 
@@ -53,25 +86,52 @@ namespace pcpp
 		/// the Destination Connection ID and Packet Number.
 		struct quic_fixed_header_short
 		{
-			/// @brief Should always be 0 for short headers
-			uint8_t headerForm : 1;
-			/// @brief Should always be 1 for valid packets.
-			/// All short headers with 0 value in this bit are considered invalid and MUST be discarded.
-			uint8_t fixedBit : 1;
-			/// @brief Latency spin bit.
-			uint8_t spinBit : 1;
+			struct
+			{
+#if (BYTE_ORDER == LITTLE_ENDIAN)
+				/// @brief The length of the Packet Number field in bytes, encoded as a 2-bit unsigned integer.
+				/// @remarks This bit is protected using header protection [QUIC-TLS Section 5.4].
+				uint8_t packetNumberLength : 2;
 
-			/// @brief Reserved bits. Must be 0 when header protection is removed.
-			/// @remarks This bit is protected using header protection [QUIC-TLS Section 5.4].
-			uint8_t reservedBits : 2;
+				/// @brief The key phase bit indicates which keys are used for packet protection.
+				/// @remarks This bit is protected using header protection [QUIC-TLS Section 5.4].
+				uint8_t keyPhaseBit : 1;
 
-			/// @brief The key phase bit indicates which keys are used for packet protection.
-			/// @remarks This bit is protected using header protection [QUIC-TLS Section 5.4].
-			uint8_t keyPhaseBit : 1;
+				/// @brief Reserved bits. Must be 0 when header protection is removed.
+				/// @remarks This bit is protected using header protection [QUIC-TLS Section 5.4].
+				uint8_t reservedBits : 2;
 
-			/// @brief The length of the Packet Number field in bytes, encoded as a 2-bit unsigned integer.
-			/// @remarks This bit is protected using header protection [QUIC-TLS Section 5.4].
-			uint8_t packetNumberLength : 2;
+				/// @brief Latency spin bit.
+				uint8_t spinBit : 1;
+
+				/// @brief Should always be 1 for valid packets.
+				/// All short headers with 0 value in this bit are considered invalid and MUST be discarded.
+				uint8_t fixedBit : 1;
+
+				/// @brief Should always be 0 for short headers
+				uint8_t headerForm : 1;
+#else
+				/// @brief Should always be 0 for short headers
+				uint8_t headerForm : 1;
+				/// @brief Should always be 1 for valid packets.
+				/// All short headers with 0 value in this bit are considered invalid and MUST be discarded.
+				uint8_t fixedBit : 1;
+				/// @brief Latency spin bit.
+				uint8_t spinBit : 1;
+
+				/// @brief Reserved bits. Must be 0 when header protection is removed.
+				/// @remarks This bit is protected using header protection [QUIC-TLS Section 5.4].
+				uint8_t reservedBits : 2;
+
+				/// @brief The key phase bit indicates which keys are used for packet protection.
+				/// @remarks This bit is protected using header protection [QUIC-TLS Section 5.4].
+				uint8_t keyPhaseBit : 1;
+
+				/// @brief The length of the Packet Number field in bytes, encoded as a 2-bit unsigned integer.
+				/// @remarks This bit is protected using header protection [QUIC-TLS Section 5.4].
+				uint8_t packetNumberLength : 2;
+#endif
+			} firstByte;
 
 			// Followed by variable-length fields:
 			// - Destination Connection ID (variable length, determined by the connection, max 20)
@@ -82,24 +142,42 @@ namespace pcpp
 	}  // namespace internal
 
 	// TODO: assign a proper value
-	// const ProtocolType QuicProtocol = 0x00;
+	// const ProtocolType QuicV0Protocol = 0x00;
+	// const ProtocolType QuicV1Protocol = 0x00;
+	// const ProtocolType QuicV2Protocol = 0x00;
 
 	class QuicLayer : public Layer
 	{
 	public:
-		QuicLayer(uint8_t* data, size_t dataLen, Layer* prevLayer, Packet* packet) 
-			: Layer(data, dataLen, prevLayer, packet, UnknownProtocol)
+		QuicLayer(uint8_t* data, size_t dataLen, Layer* prevLayer, Packet* packet)
+		    : Layer(data, dataLen, prevLayer, packet, UnknownProtocol)
 		{}
+
+		/// @brief Attempts to parse a QUIC layer from the provided buffer.
+		///
+		/// This method may return nullptr if the data does not appear to be a valid QUIC packet or if parsing fails for
+		/// any reason.
+		///
+		/// @param[in] data The raw data buffer containing the potential QUIC packet. This should point to the start of
+		/// the QUIC header.
+		/// @param[in] dataLen The length of the data buffer in bytes.
+		/// @param[in] prevLayer The previous layer in the packet.
+		/// @param[in] packet The packet to which this layer belongs.
+		/// @return A pointer to the parsed QuicLayer, or nullptr if parsing fails.
+		static QuicLayer* parseFromBuffer(uint8_t* data, size_t dataLen, Layer* prevLayer, Packet* packet);
 
 		void parseNextLayer() override
 		{
-			// Dump unparsed encrypted payload as PayloadLayer, as parsing QUIC packets is non-trivial and requires decryption.
+			// Dump unparsed encrypted payload as PayloadLayer, as parsing QUIC packets is non-trivial and requires
+			// decryption.
 		}
 
 		void parseSelf()
 		{
 			// Parse as much of the QUIC header as possible.
 			// TODO: Cache fields?
+
+			// Determine long or short form.
 		}
 
 		/// @return The header length in bytes
@@ -123,10 +201,31 @@ namespace pcpp
 		/// @return The OSI Model layer this protocol belongs to
 		OsiModelLayer getOsiModelLayer() const override
 		{
-			return OsiModelLayer::OsiModelLayerUnknown;
+			return OsiModelLayer::OsiModelTransportLayer;
 		}
+	};
 
-	private:
+	/// @brief Represents a QUIC version negotiation layer in a network packet.
+	///
+	/// See RFC 8999 and RFP 9000.
+	class QuicVerNegLayer : public QuicLayer
+	{
+		// static QuicVerNegLayer parseVerNegFromBuffer(uint8_t* data, size_t dataLen, Layer* prevLayer, Packet* packet);
+	};
 
+	/// @brief Represents a QUIC version 1 layer in a network packet.
+	///
+	/// See RFC 9000.
+	class QuicV1Layer : public QuicLayer
+	{
+		// static QuicV1Layer* parseV1FromBuffer(uint8_t* data, size_t dataLen, Layer* prevLayer, Packet* packet);
+	};
+
+	/// @brief Represents a QUIC version 2 layer in a network packet.
+	///
+	/// See RFC 9369.
+	class QuicV2Layer : public QuicLayer
+	{
+		// static QuicV2Layer* parseV2FromBuffer(uint8_t* data, size_t dataLen, Layer* prevLayer, Packet* packet);
 	};
 }  // namespace pcpp
--- Packet++/src/QuicLayer.cpp
+++ Packet++/src/QuicLayer.cpp
@@ -1,6 +1,175 @@
 #include "QuicLayer.h"
 
+#include "EndianPortable.h"
+#include "TLVData.h"
+
 namespace pcpp
 {
-	
+	namespace
+	{
+#pragma pack(push, 1)
+		/// @brief Represents a version invariant first byte of a QUIC packet header as per [RFC 8999].
+		struct quic_v0_first_byte
+		{
+#if (BYTE_ORDER == LITTLE_ENDIAN)
+			uint8_t versionSpecificBits : 7;
+			/// @brief Should always be 1 for long headers, and 0 for short headers.
+			uint8_t headerForm : 1;
+#else
+			/// @brief Should always be 1 for long headers, and 0 for short headers.
+			uint8_t headerForm : 1;
+			uint8_t versionSpecificBits : 7;
+#endif
+		};
+
+		/// @brief Represents a version invariant long header QUIC packet as per [RFC 8999].
+		struct quic_v0_fixed_header_long
+		{
+			quic_v0_first_byte firstByte;
+			uint32_t version;
+
+			// Followed by:
+			// - Destination Connection ID Length (1 byte)
+			// - Destination Connection ID (variable length, max 255 bytes)
+			// - Source Connection ID Length (1 byte)
+			// - Source Connection ID (variable length, max 255 bytes)
+			// - Version Specific Data (...)
+		};
+#pragma pack(pop)
+	}  // namespace
+
+	QuicLayer* QuicLayer::parseFromBuffer(uint8_t* data, size_t dataLen, Layer* prevLayer, Packet* packet)
+	{
+		// V1 Limits
+		// QUIC Short form header is at least 3 bytes:
+		// - 1 byte for fixed initial header.
+		// - 0 to 20 bytes for Destination connection id
+		// - 1 to 4 bytes for Packet number
+		// - 1+ bytes of protected payload. Always included due to encryption, but may be empty after decryption.
+		// TODO: Research minimum length of AEAD tag that is mandatory as part of the payload (16 bytes)?
+		constexpr size_t minViableShortFormSize = 3;
+
+		// QUIC Long form header is at least 8 bytes:
+		// - 1 for fixed initial header.
+		// - 4 byte for version
+		// - 1 byte for DCID length
+		// - 0 to 20 bytes for DCID
+		// - 1 byte for SCID length
+		// - 0 to 20 bytes for SCID
+		// - 1+ byte of type specific payload.
+		// TODO: Does it have AEAD tag?
+		constexpr size_t minViableLongFormSize = 8;
+
+		// At least 1 byte is required.
+		if (data == nullptr || dataLen < 1)
+		{
+			return nullptr;
+		}
+
+		auto* const dataEnd = data + dataLen;
+
+		quic_v0_first_byte firstByte = *reinterpret_cast<quic_v0_first_byte*>(&data[0]);
+
+		// internal::quic_common_first_byte firstByte = *reinterpret_cast<internal::quic_common_first_byte*>(&data[0]);
+
+		// Check the fixed bit. Must be 1 for valid QUIC packets. See [RFC 9000, Section 17.1 / 17.3]
+		// Version negotiation packet is the only exception.
+		// const bool fixedBitSet = firstByte.fixedBit;
+
+		// Check the form bit.
+		const bool isLongForm = firstByte.headerForm;
+		if (isLongForm)  // Long form
+		{
+			// Check the version signature of the packet:
+			constexpr uint32_t VerNegotiation = 0;
+			constexpr uint32_t Version1 = 1;
+			constexpr uint32_t Version2 = 0x6b3343cf;
+
+			uint32_t version = 0;
+			{
+				quic_v0_fixed_header_long longHeader = *reinterpret_cast<quic_v0_fixed_header_long*>(&data[0]);
+				version = be32toh(longHeader.version);
+			}
+
+			switch (version)
+			{
+			case VerNegotiation:
+			case Version1:
+			case Version2:
+				break;
+			default:
+				return nullptr;  // Failed version signature check, not a valid QUIC packet.
+			}
+
+			// Parse common data
+			// Get the location of the type specific data.
+			// Pass that to the layer for lazy eval.
+
+			if (dataLen < minViableLongFormSize)
+			{
+				return nullptr;
+			}
+
+			uint8_t* basePtr = data;
+
+			internal::quic_fixed_header_long const* longFixedHeader =
+			    reinterpret_cast<internal::quic_fixed_header_long const*>(basePtr);
+
+			uint32_t version = be32toh(longFixedHeader->version);
+
+			if (version == 0)
+			{
+				// Version negotiation packet. TODO: Support parsing this type of packet.
+				return nullptr;
+			}
+
+			if (version != 1)
+			{
+				// Only V1 is supported for now. TODO: Support parsing other versions.
+				return nullptr;
+			}
+
+			basePtr += sizeof(internal::quic_fixed_header_long);
+
+			uint8_t dcidLen = *basePtr;  // Included in minViableLongFormSize
+			basePtr += 1;
+
+			//
+			if (dcidLen > 20)
+			{
+				return nullptr;
+			}
+
+			// Out of bounds check for DCID length byte and DCID value
+			if (basePtr + dcidLen > dataEnd)
+			{
+				return nullptr;
+			}
+
+			// TODO: Length, Value
+			// LV - DCID
+			// LV - SCID
+
+			// Type Payload.
+		}
+		else  // Short form
+		{
+			// Short header does not have a version field.
+
+			// Fixed bit must be set to 1 for valid short header packets. See [RFC 9000, Section 17.2]
+			// if (!fixedBitSet)
+			// {
+			//	  return nullptr;
+			// }
+
+			if (dataLen < minViableShortFormSize)
+			{
+				return nullptr;
+			}
+
+			// TODO
+		}
+
+		return nullptr;
+	}
 }  // namespace pcpp
